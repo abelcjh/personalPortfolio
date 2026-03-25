e596fbaf3982049bb3509c077fa7fe90b1557afe
abelcjh <abelchinjh@gmail.com>
2026-03-25 23:20:31 +0800

    Updated info for awards and leadership, including URLs

---
+++ src/data/awards.ts
@@ -0,0 +1,37 @@
+import { PortfolioCardProps } from "../App.tsx";
+
+export const awards: PortfolioCardProps[] = [
+  {
+    title: "Artem Ventures Pace National Bootcamp 2025",
+    description:
+      "Chosen to be among the 120 people/startups across Malaysia to join VC Artem \
+      Ventures' second national startup bootcamp. Applications included sharing our startup \
+      ventures and interests. I was one of the youngest to join, being among the few university \
+      students starting their startup journeys. Many others are already doing startups or are \
+      working adults keen to do startups. The bootcamp trained us to learn the basics of startups, \
+      from ideation to the market. I am still at the ideation stage, but I plan to found a startup in \
+      2026 and join BLOCK71. The bootcamp recognizes the next generation of Malaysian \
+      founders, and that enables me to network with fellow like-minded peers in Malaysia's \
+      startup scene. I also join other AI and startup-related events in Malaysia. I hope to find \
+      my co-founders soon and build my unicorn across Malaysia, Singapore, and the world. \
+      With growing connections in Malaysia, I also hope to gain funding from various entities \
+      like Cradle, Malaysia’s government VC arm, and Artem.",
+    liveUrl: "https://www.linkedin.com/posts/abelchinjh_massive-shoutout-to-artem-pace-for-this-trail-blazing-activity-7404951424318484481-Oq_i",
+    liveButtonText: "View LinkedIn post"
+  },
+  {
+    title: "UNLEASH Hacks Singapore 2022 Champion",
+    description: "Won this sustainability pitching competition in Singapore. Open to everyone \
+    and my team was invited to scale our project at an incubator by GGEF. We did a project where \
+    we indicated the level of climate action of every job on LinkedIn for job seekers to find jobs \
+    that align with their climate action goals. This was done through an extension",
+    liveUrl: "https://www.facebook.com/share/p/1BvVo966P1/",
+    liveButtonText: "View Organizer Facebook post"
+  },
+  {
+    title: "GGEF SDG Hack Singapore 2022 Finalist",
+    description: "I joined this pitching competition about sustainability in Singapore. This was open to everyone, and had physical sessions. It was supported by the sustainability community in Singapore, including names like Global Green Economic Forum (GGEF). Participants had to present solutions to addresss climate change. We only formed teams on the day without knowing each other. Mentors from industry were assigned to teams. We had a few days to design our solutions, and my team had discussions off the venue too. We proposed a service that integrates with LinkedIn to reveal the carbon footprint (or contributions to climate action, I could not recall) of jobs, to raise awareness of climate action as an integral consideration for jobs, and help people gain such information easily. We won first prize where we were invited to build and scale our project at an incubator of the competition partners. I withdrew my participation as I was going to university soon and wanted to focus on my studies first.",
+    liveUrl: "https://www.youtube.com/live/vsSy_X7xYjo",
+    liveButtonText: "View Organizer YouTube livestream of Finals"
+  }
+];
---
+++ src/data/leadership.ts
@@ -0,0 +1,38 @@
+import { PortfolioCardProps } from "../App.tsx";
+
+export const leadership: PortfolioCardProps[] = [
+    {
+        title: "Nyala Labs",
+        description: "Founded and became first President of Nyala Labs, Malaysia's first \
+        registered AI youth society. Under Malaysia’s Registrar of Youths, our official name is \
+        Kelab Belia Literasi Subang Jaya, searchable in the government database (‘Subang \
+        Jaya Literacy Youth Club’ in English). ‘Nyala’ means ‘ignite’ in Malay, and stands for \
+        Network of Youths in AI Literacy & Advocacy. AI communities and school clubs exist that \
+        discuss technology, but rarely are there workshops that do actual coding integrating AI, \
+        and not constrained to some schools. We address the educational inequality among \
+        Malaysian youths in learning AI, igniting their passion in the AI space. We democratize \
+        access to in-house, self-developed AI curriculum across schools and universities in \
+        Malaysia, covering a range of topics from the basics of software development to Agentic \
+        AI tools like LangGraph. We nurture a spirit of building for good, not for profit. We have \
+        grown rapidly since founding in October 2025, where we initially started as an application \
+        to be a university club at Monash Malaysia. But with growing, diverse membership having \
+        students from different schools and regions, we went beyond the university walls and are \
+        developing as a nationwide entity. Membership in our group chat grew to over 200 in 2 \
+        months. We have established and will continue to seek partnerships with universities, VCs, \
+        government entities, among others. We continue to ensure that Nyala Labs remains the \
+        default, self-sustaining platform for Malaysian youths to access open-source AI education. \
+        One upcoming big event is a partnership with Momen, an Agentic AI startup based in San \
+        Francisco, in delivering an Agentic AI workshop. With access to at least RM 100k in \
+        government funding and facilities, we aim to reach many underprivileged groups and \
+        regions across Malaysia, working with the Ministries of Youth and Sports, and Education.
+    ",
+        liveUrl: "https://linktr.ee/nyalalabs",
+        liveButtonText: "Visit socials",
+        imgs: [
+            {
+                src: "../../public/NyalaLabsLogo.png",
+                alt: "Nyala Labs logo"
+            }
+        ]
+    },
+];
