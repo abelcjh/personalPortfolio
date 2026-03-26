56c4950e2c81b6723b031c50e9c305c8ba82e25b
Abel Chin <161250840+abelcjh@users.noreply.github.com>
2026-03-26 15:22:43 +0800

    Update awards.ts descriptions

--- src/data/awards.ts
+++ src/data/awards.ts
@@ -21,16 +21,13 @@ export const awards: PortfolioCardProps[] = [
   },
   {
     title: "UNLEASH Hacks Singapore 2022 Champion",
-    description: "Won this sustainability pitching competition in Singapore. Open to everyone \
-    and my team was invited to scale our project at an incubator by GGEF. We did a project where \
-    we indicated the level of climate action of every job on LinkedIn for job seekers to find jobs \
-    that align with their climate action goals. This was done through an extension",
+    description: "I joined this pitching competition about sustainability in Singapore. This was open to everyone, and had physical sessions. It was supported by the sustainability community in Singapore, including names like Global Green Economic Forum (GGEF). Participants had to present solutions to addresss climate change. We only formed teams on the day without knowing each other. Mentors from industry were assigned to teams. We had a few days to design our solutions, and my team had discussions off the venue too. We proposed a service that integrates with LinkedIn to reveal the carbon footprint (or contributions to climate action, I could not recall) of jobs, to raise awareness of climate action as an integral consideration for jobs, and help people gain such information easily. We won first prize where we were invited to build and scale our project at an incubator of the competition partners. I withdrew my participation as I was going to university soon and wanted to focus on my studies first.",
     liveUrl: "https://www.facebook.com/share/p/1BvVo966P1/",
     liveButtonText: "View Organizer Facebook post"
   },
   {
-    title: "GGEF SDG Hack Singapore 2022 Finalist",
-    description: "I joined this pitching competition about sustainability in Singapore. This was open to everyone, and had physical sessions. It was supported by the sustainability community in Singapore, including names like Global Green Economic Forum (GGEF). Participants had to present solutions to addresss climate change. We only formed teams on the day without knowing each other. Mentors from industry were assigned to teams. We had a few days to design our solutions, and my team had discussions off the venue too. We proposed a service that integrates with LinkedIn to reveal the carbon footprint (or contributions to climate action, I could not recall) of jobs, to raise awareness of climate action as an integral consideration for jobs, and help people gain such information easily. We won first prize where we were invited to build and scale our project at an incubator of the competition partners. I withdrew my participation as I was going to university soon and wanted to focus on my studies first.",
+    title: "GGEF SDG Open Hack Singapore 2022 Finalist",
+    description: "Another SDG-themed pitching competition. Me and my teammate pitched for a digital solution to address obesity in Malaysia. Reached finals alongside others older than us who were already at university, including a team from NUS. The final was livestreamed and still can be accessed. The competition was attended by several keu figures in Singapore's sustainability scene including representatives of GGEF.",
     liveUrl: "https://www.youtube.com/live/vsSy_X7xYjo",
     liveButtonText: "View Organizer YouTube livestream of Finals"
   }
